33a5ba2669964b2062337cedf20168872866bff6
Tomin Kappiarumalayil <62234694+tominkappiarumalayil@users.noreply.github.com>
2020-11-07 11:45:13 -0800

    Update Event.h

--- Event.h
+++ Event.h
@@ -1,7 +1,64 @@
-#ifndef EVENT_H
-#define EVENT_H
+#ifndef __EVENT_H__
+#define __EVENT_H__
 
-Event {
+#include "Location.h"
+#include "Time.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+class Event {
+    private:
+        string name;
+        string description;
+        int price;
+        Time start, end;
+        Location loc;
+    public:
+        Event();
+        Event(string &n, Time s, Time e);
+        Event(string &n, int p, Time s, Time e);
+        Event(string &n, string &d, int p, Time &s, Time &e, Location &l);
+
+        string getName() const;
+        string getDescription() const;
+        int getPrice() const;
+        Time getStartTime() const;
+        Time getEndTime() const;
+        Location getLocation() const;
+        
+        void setName(string &name);
+        void setDescription(string &description);
+        void setPrice(int price);
+        void setStartTime(Time &start);
+        void setEndTime(Time &end);
+        void setLocaion(Location &loc);
+        
+        /*
+            *****Day 1*****
+            (1pm - 2pm) - Event A
+                Location: 
+                Expected Price:
+                Description: 
+            (2pm - 3pm) - Event B
+                Location: 
+                Expected Price: 
+                Description: 
+
+            *****Day 2*****
+            (1pm - 2pm) - Event C
+                Location: 
+                Expected Price:
+                Description: 
+            (2pm - 3pm) - Event D
+                Location: 
+                Expected Price: 
+                Description: 
+
+        */
+
+        friend ostream & operator<<(ostream &out, Event &e);
 };
 
 #endif
