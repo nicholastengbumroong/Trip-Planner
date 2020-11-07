1f5fc20abf05555440698a427ab3c86daf65dd11
Tomin Kappiarumalayil <62234694+tominkappiarumalayil@users.noreply.github.com>
2020-11-07 15:33:03 -0800

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -44,8 +44,7 @@ void Event::setLocaion(const Location &l) { loc = l; }
 
 //Need Time implementation to finish this
 ostream & operator<<(ostream &out, const Event &ev) {
-    //out << "(" << ev.start << " - " << ev.end << ")  ";
-    out << ev.name << endl;
+    out << ev.name << " " << "(" << ev.start << " - " << ev.end << ")" << endl;
     out << "\t" << "Location: " << ev.loc.getStreet() << endl;
     out << "\t" << "          " << ev.loc.getCity() << ", " << ev.loc.getStateOrProvince() << endl;
     out << "\t" << "          " << ev.loc.getCountry() << endl;
