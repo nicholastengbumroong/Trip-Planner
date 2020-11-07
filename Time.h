6de652f5d673e3826a3fb20d687c0a2cf1e6ab66
Naveen Joby <74078567+navjoby@users.noreply.github.com>
2020-11-07 13:24:48 -0800

    final update to Time.h

--- Time.h
+++ Time.h
@@ -1,7 +1,48 @@
 #ifndef TIME_H
 #define TIME_H
 
-class Time {
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Time {
+    public:
+        Time();
+        Time( int Nmonth, int Nday, int Nyear );
+        Time( int Nmonth, int Nday, int Nyear, int Nhour, int Nminute, string Nampm );
+        Time( int Nhour, int Nminute );
+    
+        void setMonth( int );
+        void setDay( int );
+        void setYear( int );
+        void setHour( int );
+        void setMinute( int );
+        void setAmpm( string );
+    
+        int getMonth() const;
+        int getDay() const;
+        int getYear() const;
+        int getHour() const;
+        string getAmpm() const;
+    
+        int convertToMinutes() const;    
+    
+        operator-( Time ) const;
+    
+    private:
+        unsigned int year;
+        unsigned int month;
+        unisgned int day;
+        
+        unsigned int hour;
+        unsigned int minute;
+        string ampm;
+    
+    
+    
+    
+     
 };
 
 #endif
